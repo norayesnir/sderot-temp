2fd5d78d61fe12ce77b7806f3d511f7443f1ee7c
Rinse Yaron Schaeffer <rinseyaron@Rinses-MBP-2.owner.local>
2024-09-20 19:09:13 +0200

    Added nuxt fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/icon", "@pinia/nuxt"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/image",
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "@nuxt/fonts",
+  ],
   css: ["~/assets/styles/styles.scss"],
   vite: {
     css: {
